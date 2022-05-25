f0e7db34b330de05b01d4b12c3fada2816346112
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-05-24 22:22:18 -0300

    feat: Registro e Login de usuário

---
+++ src/NSE.Services/NSE.Identidade/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.Models;
+
+namespace NSE.Identidade.Controllers;
+
+[Route("api/identidade")]
+public class AuthController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    [HttpPost("nova-conta")]
+    public async Task<ActionResult> Registrar(UsuarioRegistro usuario)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var user = new IdentityUser
+        {
+            UserName = usuario.Email,
+            Email = usuario.Email,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, usuario.Senha);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest();
+        }
+
+        await _signInManager.SignInAsync(user: user, isPersistent: false);
+        return Ok();
+    }
+
+    [HttpPost("autenticar")]
+    public async Task<ActionResult> Login(UsuarioLogin usuario)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(
+            userName: usuario.Email,
+            password: usuario.Senha,
+            isPersistent: false,
+            lockoutOnFailure: true);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+}
