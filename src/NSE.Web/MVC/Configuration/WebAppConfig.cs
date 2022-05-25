308ee48901740f90b770007bd8711a85afc440e7
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-05-25 17:09:09 -0300

    feat: Configurando a autenticação

---
+++ src/NSE.Web/MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,37 @@
+namespace MVC.Configuration;
+
+public static class WebAppConfig
+{
+    public static void AddMvcConfiguration(this IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+    }
+
+    public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseIdentityConfiguration();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
---
+++ src/NSE.Web/MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers;
+
+public class IdentidadeController : Controller
+{
+    
+}
