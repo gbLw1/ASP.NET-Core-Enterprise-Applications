ddfe7858bd58f82de6934bcd27b1095a3fba7337
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-05-31 17:14:34 -0300

    feat: Validação de acesso baseado em claims

---
+++ src/NSE.BuildingBlocks/Services/NSE.WebAPI.Core/Identidade/CustomAuthorize.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NSE.WebAPI.Core.Identidade
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            var userIsAuthenticated = context?.User?.Identity?.IsAuthenticated ?? false;
+
+            return userIsAuthenticated && context!.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+
+    }
+
+    public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValue) };
+        }
+    }
+
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context?.HttpContext?.User?.Identity?.IsAuthenticated ?? false)
+            {
+                context!.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            if (!CustomAuthorization.ValidarClaimsUsuario(context!.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
+        }
+    }
+}
--- src/NSE.Services/NSE.Catalogo/Configurations/SwaggerConfig.cs
+++ src/NSE.Services/NSE.Catalogo/Configurations/SwaggerConfig.cs
@@ -15,6 +15,31 @@ public static class SwaggerConfig
                 Contact = new OpenApiContact { Name = "Gabriel - OnFriday", Email = "gabriel.grassi@onfriday.com.br" },
                 License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
             });
+
+            o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                Name = "Authorization",
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            o.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
         });
     }
 
