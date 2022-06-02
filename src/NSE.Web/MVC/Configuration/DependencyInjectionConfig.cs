66c50afdde983676f52c75c3f35b65dbf732a194
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-06-02 19:31:48 -0300

    feat: Entendendo os DelegatingHandlers

--- src/NSE.Web/MVC/Configuration/DependencyInjectionConfig.cs
+++ src/NSE.Web/MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,6 @@
 using MVC.Extensions;
 using MVC.Services;
+using MVC.Services.Handlers;
 
 namespace MVC.Configuration;
 
@@ -7,8 +8,12 @@ public static class DependencyInjectionConfig
 {
     public static void RegisterServices(this IServiceCollection services)
     {
+        services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
         services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-        services.AddHttpClient<ICatalogoService, CatalogoService>();
+
+        services.AddHttpClient<ICatalogoService, CatalogoService>()
+            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
---
+++ src/NSE.Web/MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,33 @@
+using System.Net.Http.Headers;
+using MVC.Extensions;
+
+namespace MVC.Services.Handlers;
+
+public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+{
+    private readonly IUser _user;
+
+    public HttpClientAuthorizationDelegatingHandler(IUser user)
+    {
+        _user = user;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+        if (!string.IsNullOrWhiteSpace(authorizationHeader))
+        {
+            request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+        }
+
+        var token = _user.ObterUserToken();
+
+        if (token is not null)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
