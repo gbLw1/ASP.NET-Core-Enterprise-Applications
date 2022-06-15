7f85e0754b4fa8549d870e7ec16150695337904b
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-06-15 16:01:49 -0300

    feat: Customizacao do EventBus - pt I

---
+++ src/NSE.BuildingBlocks/MessageBus/IMessageBus.cs
@@ -0,0 +1,33 @@
+using Core.Messages.Integration;
+using EasyNetQ.Internals;
+
+namespace MessageBus;
+
+public interface IMessageBus : IDisposable
+{
+    void Publish<T>(T message) where T : IntegrationEvent;
+
+    Task PublishAsync<T>(T message) where T : IntegrationEvent;
+
+    void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+
+    void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+
+    TResponse Request<TRequest, TResponse>(TRequest request)
+        where TRequest : IntegrationEvent
+        where TResponse : ResponseMessage;
+
+    Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+        where TRequest : IntegrationEvent
+        where TResponse : ResponseMessage;
+
+    IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
+        where TRequest : IntegrationEvent
+        where TResponse : ResponseMessage;
+
+    AwaitableDisposable<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+        where TRequest : IntegrationEvent
+        where TResponse : ResponseMessage;
+    
+    bool IsConnected { get; }
+}
