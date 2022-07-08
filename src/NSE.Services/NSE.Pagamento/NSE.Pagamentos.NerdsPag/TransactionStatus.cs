65826052d313e5bc907e1c6d46d23f2581c5799a
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-07-08 11:13:11 -0300

    feat: Entendendo o Gateway de pagamento

---
+++ src/NSE.Services/NSE.Pagamento/NSE.Pagamentos.NerdsPag/TransactionStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Pagamentos.NerdsPag;
+
+    public enum TransactionStatus
+    {
+        Authorized = 1,
+        Paid,
+        Refused,
+        Chargedback,
+        Cancelled
+    }
